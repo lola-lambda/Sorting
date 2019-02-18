780b0527932d1f8dbde5f0de29309f812ccc992f
Lola <lolahef@gmail.com>
2019-02-18 18:21:56 -0500

    Completed bubble sort & count sort

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -1,5 +1,5 @@
 # Complete the selection_sort() function below in class with your instructor
-def selection_sort( arr ):
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
@@ -16,7 +16,7 @@ def selection_sort( arr ):
         arr[cur_index] = smallest
 
     return arr
-# runtime: O(n**2)
+# runtime: O(n**2) comparisons; O(n) swaps
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort(arr):
@@ -29,20 +29,41 @@ def insertion_sort(arr):
             index -= 1
         arr[index] = cur
     return arr
+# runtime: O(n**2) comparisons & swaps
 
 # STRETCH: implement the Bubble Sort function below
-def bubble_sort( arr ):
-
+def bubble_sort(arr):
+    sorted = False
+    while (sorted is False):
+        sorted = True
+        for i in range(0, len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                sorted = False
     return arr
-
+# runtime: O(n**2) comparisons & swaps
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
-
+    m = maximum + 1
+    count = [0] * m
+    # create array of zeros that is length maximum value + 1
+    # this creates a space for the prevalence of each value in the range to be stored
+    for item in arr:
+        count[item] += 1
+    # iterate over array and increment the count (value) to represent the occurence of its index as a number in the array
+    i = 0
+    for index in range(m):
+        for item in range(count[index]):
+            arr[i] = index
+            i += 1
+    # sort the array using the prevalence of each value
     return arr
-
+# runtime: O(n + k) where k is the range of keys
 
 print('selection', selection_sort([6,5,2,1,8,19]))
 print('insertion', insertion_sort([6,5,2,1,8,19]))
-# print('bubble', bubble_sort([6,5,2,1,8,19]))
-# print('count', count_sort([6,5,2,1,8,19]))
+print('bubble', bubble_sort([6,5,2,1,8,19]))
+print('count', count_sort([6,5,2,1,8,19], 19))
