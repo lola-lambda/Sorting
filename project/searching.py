cb64609f425b06973d65a1eef43b29c8b5c8b258
Lola <lolahef@gmail.com>
2019-02-19 14:05:30 -0500

    Finished iterative binary search

--- project/searching.py
+++ project/searching.py
@@ -16,17 +16,27 @@ def binary_search(arr, target):
     
     low = 0
     high = len(arr)-1
+    
+    while low < high:
+        middle = (low+high)//2
+        if target is arr[middle]:
+            return middle
+        if target < arr[middle]:
+            high = middle
+        elif target > arr[middle]:
+            low = middle
 
     return -1 # not found
-
+# runtime complexity: O(logn)
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
   
-    middle = (low+high)/2
+    # middle = (low+high)/2
 
     if len(arr) == 0:
         return -1 # array empty
   # TO-DO: add missing if/else statements, recursive calls
 
 print('linear', linear_search([6,5,2,1,8,19], 8))
+print('iterative binary', binary_search([6,5,2,1,8,19], 8))
